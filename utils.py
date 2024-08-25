4f345cbe9351b1a20ef1b1dafbeb1850039697e3
Rehman Riyaz Ur <riyaz@connect.hku.hk>
2024-08-25 19:44:10 +0800

    fix: update

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from modelling_gemma import PaliGemmaForConditionalGeneration, PaliGemmaConfig
+from modelling_gemma import VLMConditionalGeneration, VLMConfig
 from transformers import AutoTokenizer
 import json
 import glob
@@ -9,34 +9,28 @@ import os
 
 def load_hf_model(
     model_path: str, device: str
-) -> Tuple[PaliGemmaForConditionalGeneration, AutoTokenizer]:
+) -> Tuple[VLMConditionalGeneration, AutoTokenizer]:
     tokenizer = AutoTokenizer.from_pretrained(
         model_path, padding_side="right"
     )
     assert tokenizer.padding_side == "right"
 
-    # Find all the *.safetensors files
     safetensors_files = glob.glob(os.path.join(model_path, "*.safetensors"))
 
-    # ... and load them one by one in the tensors dictionary
     tensors = {}
     for safetensors_file in safetensors_files:
         with safe_open(safetensors_file, framework="pt", device="cpu") as f:
             for key in f.keys():
                 tensors[key] = f.get_tensor(key)
 
-    # Load the model's config
     with open(os.path.join(model_path, "config.json"), "r") as f:
         model_config_file = json.load(f)
-        config = PaliGemmaConfig(**model_config_file)
+        config = VLMConfig(**model_config_file)
 
-    # Create the model using the configuration
-    model = PaliGemmaForConditionalGeneration(config).to(device)
+    model = VLMConditionalGeneration(config).to(device)
 
-    # Load the state dict of the model
     model.load_state_dict(tensors, strict=False)
 
-    # Tie weights
     model.tie_weights()
 
     return (model, tokenizer)
